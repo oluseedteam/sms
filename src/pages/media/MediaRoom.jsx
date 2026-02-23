ee0e7fa1d582334fdeebb875126c4560044348ef
abdulmuiz22 <osuolaleabdulmuiz22@gmail.com>
2026-02-23 16:50:30 +0100

    Created media room page

---
+++ src/pages/media/MediaRoom.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import rectangular from "../../assets/images/rectangular_school_collage.png";
+import { motion } from "motion/react";
+
+const MediaRoom = () => {
+  return (
+    <motion.section
+          className="relative min-h-screen  flex items-center justify-center bg-cover bg-center"
+          style={{ backgroundImage: `url(${rectangular})` }}
+          initial={{opacity: 0, y:100}}
+          transition={{duration: 1.5}}
+          whileInView={{opacity: 1, y:0}}
+          viewport={{once: false}}
+        >
+          {/* Overlay for readability */}
+          <div className="absolute blur-2xl inset-0 bg-[#0C236C4D] scale-110"></div>
+    
+          {/* Content */}
+          <div className="relative z-10 text-center text-white px-4 pt-15">
+            <h1 className="text-4xl md:text-6xl font-semibold">
+              Media Room
+            </h1>
+            <p className="mt-4 max-w-4xl mx-auto md:text-lg text-[#DFE0E5] md:text-2xl text-lg">
+              Welcome to our Media Room - your central source for school news, announcements,events, achievements and community highlights
+            </p>
+            
+            
+            </div>
+        </motion.section>
+  )
+}
+
+export default MediaRoom
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/home/Home';
 import About from '../pages/home/about/About';
+import Media from '../pages/media/Media';
+
 
 
 
@@ -12,6 +14,7 @@ const AppRoutes = () => {
       <Route element={<MainLayout/>}>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About />} />
+        <Route path='/media' element={<Media />}/>
       </Route>
     </Routes>
   )
